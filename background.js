632ee1d811c7fef071df63157b4e1b45730173fc
Salamata Bah <57270552+Salamata4255@users.noreply.github.com>
2021-06-10 21:55:41 -0400

    Update background.js

--- background.js
+++ background.js
@@ -1,1 +1,9 @@
 console.log("background is running!!");
+chrome.browserAction.onClicked.addListener(buttonClicked);
+
+function buttonClicked(tab) {
+  var msg = {
+    message: "click"
+  }
+  chrome.tabs.sendMessage(tab.id, msg);
+}
